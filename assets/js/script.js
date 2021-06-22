90a95fa5e2f42d75f3a7642d60d5c5e7ca6a7278
renatalantos <renatalantos@gmail.com>
2021-06-22 14:09:40 +0000

    Rescaling login form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,7 @@
 window.onload = startPage;
-let usernames = ["Harry", "Daisy", "Michael", "Sarah", "Sally"];
-let form = document.getElementById('registration-form');
-form.addEventListener('submit', handleSubmit);
-let errorMsg = document.getElementById('errors');
+
 let fillUsername = document.getElementById('created-name');
-let errorPwd = document.getElementById('password-error');
-let formLogin = document.getElementById('login-form');
-formLogin.addEventListener('submit', handleSubmit);
-let showLogin = document.getElementById("login");
-showLogin.addEventListener("click", onClickLogin);
-let showReg = document.getElementById("register");
-showReg.addEventListener("click", onClickReg);
-let regMsg = document.getElementById('reg-msg');
+
 let startBtn = document.getElementById('start-btn');
 startBtn.addEventListener("click", startGame);
 let buttonArea = document.getElementById('answer-area');
@@ -27,119 +17,13 @@ let mainImage = document.getElementById('main-image');
 
 
 
-function onClickLogin(event) {
-  document.getElementById("form-table2").style.visibility = "visible";
-  document.getElementById("form-table1").style.visibility = "collapse";
-
-}
-
-
-function onClickReg(event) {
-  document.getElementById("form-table1").style.visibility = "visible";
-  document.getElementById("form-table2").style.visibility = "collapse";
-
-}
-
-
-
-function clearFields() {
-  document.getElementById('email').value = '';
-  document.getElementById('username').value = '';
-  document.getElementById('password').value = '';
-  document.getElementById('repeat-password').value = '';
-  errorMsg.innerHTML = '';
-  errorPwd.innerHTML = '';
-
-
-}
-
-function clearLogin() {
-  document.getElementById('username2').value = '';
-  document.getElementById('password2').value = '';
-
-
-}
-
-
-
-
-function handleSubmit(event) {
-  event.preventDefault();
-  //let name = form.elements.username.value;
-
-  if (this.id == "registration-form") {
-
-
-    let name = document.getElementById('username').value;
-    let emailAddress = document.getElementById('email').value;
-
-    let password1 = form.elements['password'].value;
-    let password2 = form.elements['repeat-password'].value;
-
-    if ((usernames.includes(name)) && (password1 !== password2)) {
-
-      errorMsg.innerHTML = `Sorry, the username ${name} is already in use. Please choose another username.`;
-
-
-
-      errorPwd.innerHTML = "Please ensure your passwords match.";
-      errorPwd.style.display = 'block';
-
-    } else if ((usernames.includes(name)) && (password1 == password2)) {
-
-      errorMsg.innerHTML = `Sorry, the username ${name} is already in use. Please choose another username.`;
-      errorPwd.innerHTML = '';
-    } else if ((!(usernames.includes(name))) && (password1 !== password2)) {
-
-      let errorPwd = document.getElementById('password-error');
-      errorPwd.innerHTML = `Please ensure your passwords match.`;
-      errorPwd.style.display = 'block';
-      errorMsg.innerHTML = '';
-      document.getElementById("info-table").style.visibility = "collapse";
-
-    } else {
-      fillUsername.innerHTML = name;
-
-      usernames.push(name);
-      form.submit();
-      console.log(usernames);
-      clearFields();
-
-
-      regMsg.innerHTML = `Hello ${name}, thank you for registering. A confirmation email has been sent to ${emailAddress}. Enjoy playing!`;
-      regMsg.style.visibility = "visible";
-      regMsg.style.display = "block";
-
-      document.getElementById("info-table").style.visibility = "visible";
-      document.getElementById("form-table2").style.visibility = "collapse";
-      document.getElementById("form-table1").style.visibility = "collapse";
-
-    }
-
-
-  } else if (this.id = 'login-form') {
-
-
-    let nameLogin = document.getElementById('username2').value;
-    formLogin.submit();
-    fillUsername.innerHTML = nameLogin;
-    clearLogin();
-
-    document.getElementById("info-table").style.visibility = "visible";
-    document.getElementById("form-table2").style.visibility = "collapse";
-    document.getElementById("form-table1").style.visibility = "collapse";
-
-  }
-
-}
-
-
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.answer-text'));
 const scoreText = document.querySelector('#to-fill-score');
 const photoArea = document.querySelector('#photo');
 const numberOfGames = document.querySelector('#to-fill-all-games');
-//const allScore = document.querySelector('#to-fill-all-score');
+const allScore = document.querySelector('#to-fill-all-score');
+const result = document.querySelector('#result');
 
 
 let currentQuestion = {}
@@ -149,15 +33,8 @@ let questionCounter = 0;
 let availableQuestions = [];
 const score_points = 100;
 const max_questions = 15;
-//let number_of_games=0;
-//let score_in_all_games=1;
-const result = document.querySelector('#result');
-
-
-
-
-
-
+let number_of_games=0;
+let score_in_all_games=1;
 
 
 
@@ -542,9 +419,9 @@ function startGame() {
   questionArea.style.visibility = "visible";
 
 
-  function removeRegMsg() {
-    regMsg.remove();
-  }
+ // function removeRegMsg() {
+  //  regMsg.remove();
+//  }
 
   function removeMainImage() {
     mainImage.remove();
@@ -561,7 +438,7 @@ function startGame() {
   }
 
   removeMainImage();
-  removeRegMsg();
+ // removeRegMsg();
   removeMainImage();
   removeReadyQuestion();
   removeStartBtn();
@@ -578,6 +455,7 @@ getNewQuestion = () => {
 
   if ((availableQuestions.length === 0) || (questionCounter >= max_questions)) {
      localStorage.setItem('mostRecentScore', score);
+     return window.location.assign('/end.html')
 
     /* numberOfGames.innerText = ('The End!');*/
    buttonArea.style.visibility = "hidden"
@@ -609,7 +487,7 @@ getNewQuestion = () => {
 
 choices.forEach(choice => {
   choice.addEventListener('click', e => {
-    if (!acceptingAnswers) return /*???*/
+    if (!acceptingAnswers) return 
 
     acceptingAnswers = false;
     const selectedChoice = e.target;
@@ -640,7 +518,7 @@ choices.forEach(choice => {
       resDiv.append(result.innerText = "")
       console.log('set timout')
       buttonArea.classList.toggle("disabled")
-    }, 1200)
+    }, 900)
   })
 
 })
